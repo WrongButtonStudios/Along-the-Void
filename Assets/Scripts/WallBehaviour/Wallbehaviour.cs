71ef2bc5b11baf73f8bc26fd4a4b406711c19b79
Robin Kruck <Robin.Kruck93@web.de>
2024-07-24 00:21:25 +0200

    nextPush

---
+++ Assets/Scripts/WallBehaviour/Wallbehaviour.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wallbehaviour : MonoBehaviour
+{
+
+    public PlayerController playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            playerController.StartWallPause();
+        }
+    }
+
+}
