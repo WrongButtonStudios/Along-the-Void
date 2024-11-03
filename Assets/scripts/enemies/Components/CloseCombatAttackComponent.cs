c99603c9b0adaad911eec995ad94eeb58905d0f1
NilsMac <nils.henningsen@freenet.de>
2024-11-03 17:55:53 +0000

    added body check

--- Assets/scripts/enemies/Components/CloseCombatAttackComponent.cs
+++ Assets/scripts/enemies/Components/CloseCombatAttackComponent.cs
@@ -3,14 +3,51 @@ using UnityEngine;
 public class CloseCombatAttackComponent : MonoBehaviour, IAttackComponent
 {
     private SimpleAI _entity;
-
+    private bool _isCoolingDown = false;
+    private float _bodyCheckSpeed;
+    private bool _bodyCheck = false; 
     public void Attack()
     {
-        throw new System.NotImplementedException();
+        BodyCheck(); 
     }
 
     public void Init(SimpleAI entity)
     {
-        _entity = entity; 
+        _entity = entity;
+        _bodyCheckSpeed = _entity.Speed * 13f; 
     }
+
+    private void BodyCheck()
+    {
+        Vector2 pos = _entity.transform.position;
+        Vector2 ChargeDir = (_entity.PlayerPos - pos).normalized;
+
+        _entity.RB.AddForce(ChargeDir * _entity.Speed);
+
+        if ((_entity.PlayerPos - pos).sqrMagnitude < (4 * 4) && IsGrounded())
+        {
+            _entity.RB.AddForce(Vector2.up * _entity.JumpForce, ForceMode2D.Impulse);
+            _isCoolingDown = false;
+            _bodyCheck = true; 
+
+        }
+
+        if (!IsGrounded() && !_isCoolingDown && _bodyCheck)
+        {
+            _bodyCheck = false; 
+            _isCoolingDown = true;
+            _entity.RB.AddForce(ChargeDir * _bodyCheckSpeed, ForceMode2D.Impulse); 
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        if (Physics2D.Raycast(_entity.transform.position, -Vector2.up, 1.45f, ~_entity.IgnoreLayer))
+        {
+            return true;
+        }
+        return false;
+    }
+
+
 }
