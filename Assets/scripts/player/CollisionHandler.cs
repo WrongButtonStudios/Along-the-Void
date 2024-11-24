c98d0cc4e3fac5bfe572cbd821a83ed64eea8872
nilsh <nils.henningsen@freenet.de>
2024-11-24 15:50:52 +0100

    started damage dealing via red spikes

--- Assets/scripts/player/CollisionHandler.cs
+++ Assets/scripts/player/CollisionHandler.cs
@@ -1,16 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement; 
 
 public class CollisionHandler : MonoBehaviour
 {
+    private fairyController _fairyController = null;
+    private characterController _cc;
+    
+    private enum PlayerColor 
+    {
+        red, 
+        green, 
+        blue, 
+        yellow,
+        unknown
+    }
+
+    private void Start()
+    {
+        _fairyController = GameObject.FindObjectOfType<fairyController>();
+        _cc = this.gameObject.GetComponent<characterController>(); 
+    }
     public void OnTriggerEnter2D(UnityEngine.Collider2D collision)
     {
         Debug.Log("trigger entered"); 
         if (CompareName("SpikeRed", collision.gameObject)) 
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+            var playerColor = GetPlayerColor();
+            switch(playerColor) 
+            {
+                case PlayerColor.red:
+                    break;
+                case PlayerColor.green:
+                    break;
+                case PlayerColor.blue:
+                    break;
+                case PlayerColor.yellow:
+                    break;
+                case PlayerColor.unknown:
+                    Debug.LogError("Couldnt deal Damage: Unknown Color");
+                    return; 
+            }
         }
     }
 
@@ -23,5 +56,27 @@ public class CollisionHandler : MonoBehaviour
             otherName += fullName[i]; 
         }
         return otherName==name; 
+    }
+
+    private PlayerColor GetPlayerColor() 
+    {
+        if (_cc.getPlayerStatus().currentState == characterController.playerStates.red || _cc.getPlayerStatus().currentState == characterController.playerStates.burntRed)
+        {
+            return PlayerColor.red;
+        }
+        else if (_cc.getPlayerStatus().currentState == characterController.playerStates.green || _cc.getPlayerStatus().currentState == characterController.playerStates.burntGreen)
+        {
+            return PlayerColor.green;
+        }
+        else if (_cc.getPlayerStatus().currentState == characterController.playerStates.blue || _cc.getPlayerStatus().currentState == characterController.playerStates.burntBlue)
+        {
+            return PlayerColor.blue;
+        }
+        else if (_cc.getPlayerStatus().currentState == characterController.playerStates.yellow || _cc.getPlayerStatus().currentState == characterController.playerStates.burntYellow)
+        {
+            return PlayerColor.yellow;
+        }
+        else
+            return PlayerColor.unknown; 
     }
 }
