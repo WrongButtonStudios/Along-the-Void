9158fe9798b6f3e67473e8d44a53d70c1374a7e1
NilsMac <nils.henningsen@freenet.de>
2024-12-15 17:58:20 +0100

    Scene unloads, player is marked as dontdestroyonload and survives the unload. He's build diffrerent damn

--- Assets/scripts/levelLoading/LoadNextScene.cs
+++ Assets/scripts/levelLoading/LoadNextScene.cs
@@ -13,25 +13,32 @@ public class LoadNextScene : MonoBehaviour
     private Transform _levelStart;
     [SerializeField]
     private Transform _levelEnd;
-    private Vector2 _offset; 
+    private Vector2 _offset;
+    private bool _canBeTriggered = false;
+    private bool _finnished = false;
+
 
     private void Start()
     {
         if(_loadScene)
-            _offset = ((Vector2)_levelEnd.position - (Vector2)_levelStart.position)/2; 
+            _offset = ((Vector2)_levelEnd.position - (Vector2)_levelStart.position) *0.75f;
+        StartCoroutine(WaitForActivation()); 
     }
 
     private void LoadInLevel()
     {
+        if (_finnished)
+            return;
+        _finnished = true; 
         Debug.Log(_sceneToLoad); 
         Debug.Log("Guck mal ich lad ne scene");
         SceneManager.LoadSceneAsync(_sceneToLoad, LoadSceneMode.Additive).completed += operation =>
         {
-            Scene loadedScene = SceneManager.GetSceneByName(_sceneToLoad);
+            Scene scene = SceneManager.GetSceneByName(_sceneToLoad);
 
-            if (loadedScene.IsValid())
+            if (scene.IsValid())
             {
-                foreach (GameObject obj in loadedScene.GetRootGameObjects())
+                foreach (GameObject obj in scene.GetRootGameObjects())
                 {
                     obj.transform.position += (Vector3)_offset;
                 }
@@ -43,17 +50,27 @@ public class LoadNextScene : MonoBehaviour
     private void UnloadLevel()
     {
         Debug.Log("Guck mal ich entlade ne scene, hihi ich hab entladen gesagt");
-        SceneManager.UnloadSceneAsync(_sceneToLoad); 
+        SceneManager.UnloadSceneAsync(_sceneToLoad);
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<characterController>())
+        if (_canBeTriggered)
         {
-            if (_loadScene)
-                LoadInLevel();
-            else
-                //UnloadLevel(); 
+             if (collision.GetComponent<characterController>())
+             {
+                 if (_loadScene)
+                     LoadInLevel();
+                 else
+                     UnloadLevel(); 
+             }
         }
+    }
+
+    IEnumerator WaitForActivation()
+    {
+        yield return new WaitForSeconds(2);
+        _canBeTriggered = true; 
     }
 }
