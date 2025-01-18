1aaebbc68e72b15c531bab6bd6ba24b832b242d8
NilsMac <nils.henningsen@freenet.de>
2025-01-18 17:21:00 +0100

    Restructured Component Select functioned, renamed class and AttackComponentSelecter function for better clearity

--- Assets/scripts/enemies/EnemyComponentSelecter.cs
+++ Assets/scripts/enemies/EnemyComponentSelecter.cs
@@ -2,29 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyComponentSelecter : MonoBehaviour
+public class EnemyBehaviourComponentSelecter : MonoBehaviour
 {
-    public static sbyte SelectNewWeapon(SimpleAI entity, sbyte curIndex)
+    public static sbyte SelectAttackComponent(SimpleAI entity, sbyte curIndex)
     {
-        if (entity.AttackComponents[curIndex].FinnishedAttack())
-        {
-            sbyte oldWeapon = curIndex;
-            sbyte newWeaponIndex = (sbyte)Random.Range(0, entity.AttackComponents.Count - 1);
-            entity.AttackComponents[oldWeapon].ResetAttackStatus();
-            return newWeaponIndex; 
-        }
-        //enemy is still attacking
-        return -1; 
+        if (entity.AttackComponents[curIndex].FinnishedAttack() == false)
+            return curIndex;
+
+        entity.AttackComponents[curIndex].ResetAttackStatus();
+        sbyte newWeaponIndex = (sbyte)Random.Range(0, entity.AttackComponents.Count - 1);
+        return newWeaponIndex; 
     }
 
-    public static sbyte SelectMovementComponent(SimpleAI entity, int curIndex)
+    public static sbyte SelectMovementComponent(SimpleAI entity, sbyte curIndex)
     {
-        if (entity.PatrolComponents[curIndex].ReachedDestination())
-        {
-            return (sbyte)Random.Range(0, entity.PatrolComponents.Count - 1);
-        }
-        //Enemy hasnt reached destination
-        return -1; 
-    }
+        if (entity.PatrolComponents[curIndex].ReachedDestination() == false)
+            return curIndex; 
 
+        return (sbyte)Random.Range(0, entity.PatrolComponents.Count - 1);
+    }
 }
