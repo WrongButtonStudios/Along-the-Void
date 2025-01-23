f05afcbe0f51007c48fb6775ab0f360341756e86
Julian-Haas <julian.haas@web.de>
2025-01-23 08:17:48 +0100

    fixed color not changing when empty

--- Assets/scripts/player/fairy.cs
+++ Assets/scripts/player/fairy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class fairy : MonoBehaviour
@@ -16,13 +17,15 @@ public class fairy : MonoBehaviour
     public class fairyData
     {
         public fairyColor color;
-        [Range(0, 1)]public float colorAmount;
+        [Range(0,1)] public float colorAmount;
         public GameObject fairyObject;
     }
 
     public GameObject shape;
     public SpriteRenderer spriteRenderer;
     public SpriteMask spriteMask;
+
+    public bool empty = false;
 
     private fairyController fairyController;
     private float colorAmount;
@@ -34,28 +37,27 @@ public class fairy : MonoBehaviour
     [SerializeField] private Color blueColor;
     [SerializeField] private Color yellowColor;
 
-    private void Start()
-    {
+    private void Start() {
         fairyController = GetComponentInParent<fairyController>();
     }
 
-    private void Update()
-    {
-        if (fairyDataIndex != -1)
-        {
-            if (fairyController.fairys[fairyDataIndex].colorAmount != colorAmount)
-            {
+    private void Update() {
+        if(fairyDataIndex != -1) {
+            if(fairyController.fairys[fairyDataIndex].colorAmount != colorAmount) {
                 colorAmount = fairyController.fairys[fairyDataIndex].colorAmount;
 
                 Vector3 newMaskPosition = spriteMask.transform.localPosition;
-                newMaskPosition.y = Mathf.Lerp(-spriteMask.transform.localScale.x, 0f, colorAmount);
+                newMaskPosition.y = Mathf.Lerp(-spriteMask.transform.localScale.x,0f,colorAmount);
                 spriteMask.transform.localPosition = newMaskPosition;
             }
         }
+        if(colorAmount <= 0.0f && !empty) {
+            fairyController.SwitchColorBecauseOfEmptyColorThatsWhy();
+            empty = true;
+        }
     }
 
-    public void setIndex(int index)
-    {
+    public void setIndex(int index) {
         fairyDataIndex = index;
 
         spriteRenderer.sortingLayerName = "fairyMask" + index;
@@ -63,10 +65,8 @@ public class fairy : MonoBehaviour
         spriteMask.backSortingLayerID = SortingLayer.NameToID("fairyMask" + (index - 1));
     }
 
-    public void setColor(fairyColor colorToSet)
-    {
-        switch (colorToSet)
-        {
+    public void setColor(fairyColor colorToSet) {
+        switch(colorToSet) {
             case fairyColor.green:
                 spriteRenderer.color = greenColor;
                 break;
