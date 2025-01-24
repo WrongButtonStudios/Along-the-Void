88596b54edd17a548d9625cc39fd602966d5c012
NilsMac <nils.henningsen@freenet.de>
2025-01-24 14:17:28 +0100

    fixed color drop

--- Assets/scripts/levelObjects/ColorDrop.cs
+++ Assets/scripts/levelObjects/ColorDrop.cs
@@ -21,18 +21,22 @@ public class ColorDrop : MonoBehaviour
     }
     private void HealLowestFairy()
     {
+        int i = 0; 
         float lowestHP = float.MaxValue;
         fairy.fairyData fairyToHeal = new fairy.fairyData(); 
         foreach (fairy.fairyData hp in _fairyController.fairys)
         {
-            if (lowestHP > hp.colorAmount)
+            if (lowestHP < hp.colorAmount)
             {
-                fairyToHeal = hp;
-                lowestHP = hp.colorAmount; 
+                Debug.Log("Bin raus " + i); 
+                continue;
             }
+            i++; 
+            lowestHP = hp.colorAmount; 
         }
-        fairyToHeal.colorAmount = 1f;
-        Destroy(this.gameObject); 
 
+        _fairyController.fairys[i].colorAmount = 1f;
+        Debug.Log("Habe Fairy: " + _fairyController.fairys[i].color + "Aufgefüllt"); 
+        Destroy(this.gameObject); 
     }
 }
